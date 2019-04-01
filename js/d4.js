19f1c7225cef838ef0718d090442cb525331aae8
Ngai To <ngai_to_lo@hotmail.com>
2019-03-31 19:24:56 -0700

    added barchart

--- js/d4.js
+++ js/d4.js
@@ -1,3 +1,74 @@
+
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+width = 600 - margin.left - margin.right,
+height = 300 - margin.top - margin.bottom;
+
+// Parse the date / time
+var	parseDate = d3.time.format("%Y-%m").parse;
+
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+var xAxis = d3.svg.axis()
+.scale(x)
+.orient("bottom")
+.tickFormat(d3.time.format("%Y-%m"));
+
+var yAxis = d3.svg.axis()
+.scale(y)
+.orient("left")
+.ticks(10);
+
+
+var svg1 = d3.select("#chart").append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", 
+      "translate(" + margin.left + "," + margin.top + ")");
+// load data
+d3.csv("./data/bar-data.csv", function(error, data) {
+
+data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.value = +d.value;
+});
+
+x.domain(data.map(function(d) { return d.date; }));
+y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+svg1.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis)
+.selectAll("text")
+  .style("text-anchor", "end")
+  .attr("dx", "-.8em")
+  .attr("dy", "-.55em")
+  .attr("transform", "rotate(-90)" );
+
+svg1.append("g")
+  .attr("class", "y axis")
+  .call(yAxis)
+.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("Value ($)");
+
+svg1.selectAll("bar")
+  .data(data)
+.enter().append("rect")
+  .style("fill", "steelblue")
+  .attr("x", function(d) { return x(d.date); })
+  .attr("width", x.rangeBand())
+  .attr("y", function(d) { return y(d.value); })
+  .attr("height", function(d) { return height - y(d.value); });
+
+});
+
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     padding = {top: 0, right: 0, bottom: 30, left: 50},
     width = 860 - margin.left - margin.right,
@@ -27,7 +98,7 @@ var cValue = function(d) { return d.Manufacturer;},
     color = d3.scale.category10();
 
 // add the graph canvas to the body of the webpage
-var svg = d3.select("#chart").append("svg")
+var svg = d3.select("#chart2").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
