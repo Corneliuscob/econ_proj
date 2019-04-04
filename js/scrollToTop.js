3f7293ca6fd139fe7f827ef506d9926ec06bcc29
Ngai To <ngai_to_lo@hotmail.com>
2019-04-04 00:38:46 -0700

    added appendix need to check nubmers more css too

---
+++ js/scrollToTop.js
@@ -0,0 +1,16 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 50; // For Safari
+  document.documentElement.scrollTop = 50; // For Chrome, Firefox, IE and Opera
+} 
